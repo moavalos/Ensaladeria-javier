a7c99fb66b1e65643abff360ac5ace801ce7909b
mora avalos <moavalos@alumno.unlam.edu.ar>
2022-05-30 23:24:31 -0300

    paso 1

--- src/dominio/Ensalada.java
+++ src/dominio/Ensalada.java
@@ -1,18 +1,20 @@
 package dominio;
 
+import java.util.Arrays;
+
 public class Ensalada {
 
-
-/*	private String nombreDeLaEnsalada;
-	private Ingrediente ingredientes[];
-	//private Integer contadorIngredientes = 0; ???????
+	private String nombreDeLaEnsalada;
+	private Ingredientes ingredientes[];
+	private Integer contadorIngredientes = 0;
+	// no lucas basta
 
 	public Ensalada(String nombreDeLaEnsalada) {
 		this.nombreDeLaEnsalada = nombreDeLaEnsalada;
-		this.ingredientes = new Ingrediente[20];
+		this.ingredientes = new Ingredientes[20];
 	}
 
-	public Ingrediente[] getIngredientes() {
+	public Ingredientes[] getIngredientes() {
 		return this.ingredientes;
 	}
 
@@ -20,23 +22,48 @@ public class Ensalada {
 		return this.nombreDeLaEnsalada;
 	}
 
-	public Boolean agregarIngrediente(Ingrediente nuevo) {
-	
+	public Boolean agregarIngrediente(Ingredientes nuevo) {
+		this.ingredientes[contadorIngredientes++] = nuevo;
+		return true;
 	}
 
-	public Boolean esAptoCeliaco() {
-	
+	public Boolean paraCeliacos() {
+		return false;
 	}
 
-	public Boolean esAptoVegetariano() {
-
+	public Boolean paraVegetarianos() {
+		return false;
 	}
 
-	public boolean esAptoVegano() {
-	
+	public boolean paraVeggies() { // emoji con ojos estrella
+		return false;
 	}
 
+	@Override
 	public String toString() {
+		return "Ensalada [nombreDeLaEnsalada=" + nombreDeLaEnsalada + ", ingredientes=" + Arrays.toString(ingredientes)
+				+ ", contadorIngredientes=" + contadorIngredientes + "]";
 	}
-*/
+
+	public String getNombreDeLaEnsalada() {
+		return nombreDeLaEnsalada;
+	}
+
+	public void setNombreDeLaEnsalada(String nombreDeLaEnsalada) {
+		this.nombreDeLaEnsalada = nombreDeLaEnsalada;
+	}
+
+	public Integer getContadorIngredientes() {
+		return contadorIngredientes;
+	}
+
+	public void setContadorIngredientes(Integer contadorIngredientes) {
+		this.contadorIngredientes = contadorIngredientes;
+	}
+
+	public void setIngredientes(Ingredientes[] ingredientes) {
+		this.ingredientes = ingredientes;
+	}
+
+	
 }
--- src/dominio/Pedido.java
+++ src/dominio/Pedido.java
@@ -1,25 +1,25 @@
 package dominio;
 
 public class Pedido {
-	/*
-
+	
 	private String nombreDelCliente;
 	private Ensalada[] ensaladas;
 	private Integer contadorEnsaladas;
 
 	public Pedido(String nombreDelCliente) {
-		
+		this.nombreDelCliente = nombreDelCliente;
+		this.ensaladas = new Ensalada[100];
 	}
 
 	public Boolean agregarNuevaEnsalada(Ensalada nueva) {
-		
+		this.ensaladas[this.contadorEnsaladas++] = nueva;
+		return true;
 	}
 
 	public Double getImporte() {
-		
+		/* calcula el importe en funcion del precio de cada ingrediente */
+		return 0.0;
 	}
 
-	public Ensalada buscarEnsalada(String nombreEnsalada) {
-		
-	}*/
+
 }
